377fe6c8ba6bd859a1587e63e2347de52d1ddb23
Parfyonov Roman <r.parfen05@gmail.com>
2018-03-18 13:55:26 +0200

    Initial commit

---
+++ src/com/company/Apple.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by Student on 18.03.2018.
+ */
+public enum Apple {
+    Jonathan, GoldenDel, RedDel, Whitesap, Cortland
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Apple ap, ap2, ap3;
+        ap = Apple.RedDel;
+
+        System.out.println("Value of app: " + ap);
+        System.out.println();
+
+        ap = Apple.Jonathan;
+
+
+
+
+       /* Apple allapples[] = Apple.values();
+        for (Apple a : allapples) {
+            System.out.println(a);
+        }
+
+
+        System.out.println();
+
+        ap = Apple.valueOf("Whitesap");
+        System.out.println("ap contains " + ap);
+
+        for (Apple a : Apple.values()) {
+            System.out.println(a + " " + a.ordinal());
+        }*/
+        ap = Apple.RedDel;
+        ap2 = Apple.GoldenDel;
+        ap3 = Apple.RedDel;
+
+       /* if (ap.compareTo(ap2) < 0) {
+            System.out.println(ap + " comes before " + ap2);
+        }
+
+        if (ap.compareTo(ap2) > 0) {
+            System.out.println(ap2 + " comes before " + ap);
+        }
+
+        if (ap.compareTo(ap3) == 0) {
+            System.out.println(ap + " equals " + ap3);
+        }
+
+        System.out.println();
+
+        if (ap.equals(ap2)) {
+            System.out.println("Error!");
+        }
+        if (ap.equals(ap3)) {
+            System.out.println(ap + " equals " + ap3);
+        }
+
+        if (ap == ap3) {
+            System.out.println(ap + " == " + ap3);
+        }*/
+
+       /* if (ap == Apple.GoldenDel) {
+            System.out.println("ap contains GoldenDel.\n");
+        }
+*/
+        /*switch (ap) {
+            case Jonathan:
+                System.out.println("Jonathan is red.");
+                break;*/
+    }
+
+}
+
